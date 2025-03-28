afd82d5cb54f6594e8163ea0518bd196cd6bf911
David Saah <davesaah@gmail.com>
2025-03-28 12:13:50 +0000

    Add server middleware for logging, recovery and CORS

--- main.go
+++ main.go
@@ -2,10 +2,27 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	api := echo.New()
+
+	api.Use(middleware.Recover())   // recover
+	api.Use(middleware.RequestID()) // add request ID
+	api.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: `{"time":"${time_rfc3339_nano}","id":"${id}","remote_ip":"${remote_ip}",` +
+			`"host":"${host}","method":"${method}","uri":"${uri}","user_agent":"${user_agent}",` +
+			`"status":${status},"error":"${error}","latency_human":"${latency_human}",}` + "\n",
+	})) // logger
+
+	// Enable CORS
+	api.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{echo.GET, echo.POST, echo.PATCH, echo.DELETE},
+		AllowHeaders:     []string{echo.HeaderAuthorization, echo.HeaderContentType},
+		AllowCredentials: true,
+	}))
 
 	api.POST("/register", register)
 	api.GET("/register", registerValidation)
